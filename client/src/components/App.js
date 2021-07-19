3dace15a8397c6c8b435d7f307691b5cfa6fbb5d
Luca TC <lucatuveri@outlook.it>
2021-07-19 20:45:15 +0200

    solved changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,18 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-<<<<<<< HEAD
 import UserList from './pages/UsersPage/UsersList'
-import ProfilePageItemsList from './pages/UsersPage/ProfilePageItemsList'
+import ProfilePageItemsList from './pages/UserProfilePageItemsList/ProfilePageItemsList'
 
-=======
 import UserProfile from './pages/UserProfilePage/UserProfile'
->>>>>>> 4eb69a18c470dce085333341672770a01285e1d7
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <h1>yay!</h1>
       <UserList />
       <hr></hr>
       <ProfilePageItemsList />
-=======
       <UserProfile />
->>>>>>> 4eb69a18c470dce085333341672770a01285e1d7
     </>
   )
 }
