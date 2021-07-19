4dcda59eb798a3cc094e46304af8fc5cef9efe61
CarolinaVitale <vitale.carola@gmail.com>
2021-07-19 16:12:08 +0200

    components

--- client/src/components/pages/UsersPage/UserListCard.js
+++ client/src/components/pages/UsersPage/UserListCard.js
@@ -1,13 +1,14 @@
 import { Col, Card } from "react-bootstrap"
 
-const UserListCard = ({ email, firstName, lastName, _id }) => {
+const UserListCard = ({ email, firstName, lastName, cover, image, _id }) => {
+    
     return (
-
         <Col md={4}>
             <Card className="user-card">
                 <Card.Body>
                     <Card.Title>{firstName} {lastName}</Card.Title>
                     <p>User: {email} </p>
+                    <img src={cover} />
                 </Card.Body>
             </Card>
         </Col>
