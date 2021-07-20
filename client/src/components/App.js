e3c48620d8568feba8710d842ea3899b09412aa4
Luca TC <lucatuveri@outlook.it>
2021-07-20 12:00:36 +0200

    12:00

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,20 +8,11 @@ import UserProfile from './pages/UserProfilePage/UserProfile'
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <h1>yay!</h1>
-      <UserList />
-      <hr></hr>
-      <ProfilePageItemsList />
-      <UserProfile />
-      <Navbar />
-=======
-      <Navbar />
-      <br></br>
       <UserProfile />
       <br></br>
+      <Navbar />
+      <br></br>
       <ProfilePageItemsList />
->>>>>>> 16e6ac245498754f94334bd267442168033b03ab
     </>
   )
 }
--- client/src/components/pages/UserProfilePageItemsList/ProfilePageItemsList.js
+++ client/src/components/pages/UserProfilePageItemsList/ProfilePageItemsList.js
@@ -1,11 +1,9 @@
 import { Component } from 'react'
-import { Row, Modal } from "react-bootstrap"
+import { Row, Container } from "react-bootstrap"
 
 import UserService from '../../../services/users.service'
 import ProfilePageItemsListCard from './ProfilePageItemsCard'
-import UserProfileCard from './../UserProfilePage/UserProfilePage'
 
-import './UserListCard.css'
 
 class ProfilePageItemsList extends Component {
 
@@ -15,14 +13,16 @@ class ProfilePageItemsList extends Component {
             users: undefined,
             modal: false
         }
+
         this.userService = new UserService()
     }
 
 
+
     loadUser = () => {
+
         this.userService
             .getUsers()
-
             .then(response => this.setState({ users: response.data }))
             .catch(err => console.log(err))
     }
@@ -31,30 +31,22 @@ class ProfilePageItemsList extends Component {
 
     componentDidMount = () => {
         this.loadUser()
-
     }
 
 
     render() {
+
         return (
             !this.state.users
                 ? <h3>Cargando...</h3>
                 :
                 (
                     <>
-                        <Row className="justify-content-center">
-                            <p onClick={() => this.setState({ modal: true })} >presshere to see modal that doesn't works in cards</p>
-                            {this.state.users.map((elm, modal) => <ProfilePageItemsListCard onClick={() => this.setState({ modal: true })} key={elm._id} {...elm} />)}
-                        </Row>
-
-                        <Modal show={this.state.modal} onHide={() => this.setState({ modal: false })}>
-                            <Modal.Header>
-                                <Modal.Title>Preview</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <UserProfileCard refreshItems={this.loadUser} closeModal={() => this.setState({ modal: false })} />
-                            </Modal.Body>
-                        </Modal>
+                        <Container>
+                            <Row>
+                                {this.state.users.map((elm, modal) => <ProfilePageItemsListCard key={elm._id} {...elm} />)}
+                            </Row>
+                        </Container>
                     </>
                 )
         )
