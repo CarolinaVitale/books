d50c88596c130df865c482e3adde2c1dc64bb0d5
Luca TC <lucatuveri@outlook.it>
2021-07-20 12:15:38 +0200

    12:15

--- client/src/components/pages/UserProfilePageItemsList/ProfilePageItemsList.js
+++ client/src/components/pages/UserProfilePageItemsList/ProfilePageItemsList.js
@@ -42,19 +42,6 @@ class ProfilePageItemsList extends Component {
                 :
                 (
                     <>
-                        <Row className="justify-content-center">
-                            <p onClick={() => this.setState({ modal: true })} >presshere to see modal that doesn't work in cards</p>
-                            {this.state.users.map((elm, modal) => <ProfilePageItemsListCard onClick={() => this.setState({ modal: true })} key={elm._id} {...elm} />)}
-                        </Row>
-
-                        <Modal show={this.state.modal} onHide={() => this.setState({ modal: false })}>
-                            <Modal.Header>
-                                <Modal.Title>Preview</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <UserProfileCard refreshItems={this.loadUser} closeModal={() => this.setState({ modal: false })} />
-                            </Modal.Body>
-                        </Modal>
                         <Container>
                             <Row>
                                 {this.state.users.map((elm, modal) => <ProfilePageItemsListCard key={elm._id} {...elm} />)}
