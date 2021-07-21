1ff8f8af0f13aef0549a3e4926d11d335501c4dd
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-07-21 11:38:37 +0200

    CodeReview

--- client/src/components/routes/index.js
+++ client/src/components/routes/index.js
@@ -1,11 +1,12 @@
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Profile from '../pages/ProfilePage/Profile'
 
 const Routes = () => {
 
     return (
         <Switch>
             {/* <Route path='/' exact render={() => <HomePage />} /> */}
-            <Route path='/' exact render={() => <HomePage />} />
+            <Route path='/' exact render={() => <Profile />} />
 
         </Switch>
     )
