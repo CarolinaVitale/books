ce22d29c3dce9195bc88f9ea46df4850be8ac995
Luca TC <lucatuveri@outlook.it>
2021-07-22 17:01:18 +0200

    este login modal

--- client/src/components/routes/index.js
+++ client/src/components/routes/index.js
@@ -14,7 +14,7 @@ const Routes = ({ storeUser, loggedUser }) => {
 
     return (
         <Switch>
-            <Route path='/' exact render={() => <HomePage />} />
+            <Route path='/' exact render={() => <HomePage loggedUser={loggedUser} />} />
             <Route path="/register" render={props => <Register {...props} />} />
             <Route path="/login" render={props => <Login {...props} storeUser={storeUser} />} />
             <Route path="/profile" render={() => loggedUser ? <MyProfile loggedUser={loggedUser} /> : <Redirect to="/login" />} />
